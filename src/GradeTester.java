d527ea24230396d97949c2ce9c7dfba94536b1b1
Owei88 <89204505+Owei88@users.noreply.github.com>
2022-01-11 17:55:35 -0700

    Update GradeTester.java

--- src/GradeTester.java
+++ src/GradeTester.java
@@ -9,6 +9,8 @@ public class GradeTester {
         studentGrade.getNumericGrade(studentGPA);
         System.out.println("This should get the student's letter grade based on collegeboard letter to gpa system " +
                 "For example, 4.0 is A, 3.7 and above is A-, 3.3 and above is B+, etc. ");
+        System.out.println("For an input of 3.65, it should output B+");
+        System.out.println("For an input of 4.0, it should output A");
 
     }
 }
